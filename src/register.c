f50879b7808b0cf2658197c84a2c085fbe1db9e8
Huy Nguyen <huy55465@gmail.com>
2024-04-22 23:37:18 -0400

    fix: remove unnecessary variables and conditions in test register

--- src/register.c
+++ src/register.c
@@ -8,7 +8,6 @@ typedef struct test_node test_node;
 struct test_node {
   void (*test)();
   const char *filename;
-  bool run;
   int line;
 };
 
@@ -19,15 +18,11 @@ static int n_test = 0;
 void reglist_add(void (*testname)(), const char *filename, int line) {
   reglist[n_test].test = testname;
   reglist[n_test].filename = filename;
-  reglist[n_test].run = true;
   reglist[n_test].line = line;
   n_test++;
 }
 
-void reglist_exclude_newest() {
-  if (n_test > 0)
-    n_test--;
-}
+void reglist_exclude_newest() { n_test--; }
 
 void run_all_tests() {
   for (int i = 0; i < n_test; i++) {
