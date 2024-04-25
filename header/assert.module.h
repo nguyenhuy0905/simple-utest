a2740cbb41043e7d360b714b4f4f028c2759049e
Huy Nguyen <huy55465@gmail.com>
2024-04-25 17:59:22 -0400

    feat: add an assert module to reduce repetition

---
+++ header/assert.module.h
@@ -0,0 +1,22 @@
+#ifndef __SIMPLE_UTEST_ASSERT_MODULE_H__
+#define __SIMPLE_UTEST_ASSERT_MODULE_H__
+
+#define log_if_success(comparer, format)                                       \
+  if (comparer) {                                                              \
+    if ((verbosity >> 1) & 0b1)                                                \
+      log_success(format);                                                     \
+    return;                                                                    \
+  }
+
+#define log_if_fail(format)                                                    \
+  notify_fail();                                                               \
+  if (!verbosity)                                                              \
+    return;                                                                    \
+  log_fail(format);
+
+#define set_up_assert(comparer, format)                                        \
+  uint16_t verbosity = get_verbosity();                                        \
+  log_if_success(comparer, format);                                            \
+  log_if_fail(format);
+
+#endif // !__SIMPLE_UTEST_ASSERT_MODULE_H__
--- src/assert.c
+++ src/assert.c
@@ -1,43 +1,17 @@
 #include "../header/assert.h"
+#include "../header/assert.module.h"
 #include "../header/log.h"
 #include "../simple-utest.h"
 #include <string.h>
 
 void _simple_assert_int(int expected, int actual, int line) {
-  uint16_t verbosity = get_verbosity();
-  if (expected == actual) {
-    if ((verbosity >> 1) & 0b1)
-      log_success("%d");
-    return;
-  }
-  notify_fail();
-  if (!verbosity)
-    return;
-  log_fail("%d");
+  set_up_assert(expected == actual, "%d");
 }
 
 void _simple_assert_double(double expected, double actual, int line) {
-  uint16_t verbosity = get_verbosity();
-  if (expected == actual) {
-    if ((verbosity >> 1) & 0b1)
-      log_success("%f");
-    return;
-  }
-  notify_fail();
-  if (!verbosity)
-    return;
-  log_fail("%f");
+  set_up_assert(expected == actual, "%f");
 }
 
 void _simple_assert_string(const char *expected, const char *actual, int line) {
-  uint16_t verbosity = get_verbosity();
-  if (strcmp(expected, actual) == 0) {
-    if ((verbosity >> 1) & 0b1)
-      log_success("%s");
-    return;
-  }
-  notify_fail();
-  if (!verbosity)
-    return;
-  log_fail("%s");
+  set_up_assert(strcmp(expected, actual) == 0, "%s");
 }
