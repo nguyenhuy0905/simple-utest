5c96fd732dd65bff1ea95f4f7bf74200f8dc5940
Huy Nguyen <huy55465@gmail.com>
2024-04-25 16:32:32 -0400

    feat: change demo file(s) in accordance to new features

--- test/test-something.c
+++ test/test-something.c
@@ -5,12 +5,13 @@ register_test(test_something, EXCLUDE | VERBOSE) {
   simple_assert_string("Never gonna give you up", "Never gonna let you down");
 }
 
-register_test(test_something_else, VERBOSE) {
+register_test(test_something_else, VERBOSE_FAIL_LOG) {
   simple_assert_int(1, 2);
   simple_assert_int(69, 70);
+  simple_assert_int(6, 6);
 }
 
-register_test(test_something_true, DEFAULT){
+register_test(test_something_true, VERBOSE_SUCCESS_LOG) {
   simple_assert_double(0.0, 0.0);
   simple_assert_string("Hello world", "Hello world");
 }
